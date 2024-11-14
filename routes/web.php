b6df6070a708662cf72bd0c688f52ee3e1d18469
Andrew <149857208+PrimeBeyonder@users.noreply.github.com>
2024-11-14 23:28:47 +0630

    CSRF toten

--- routes/web.php
+++ routes/web.php
@@ -8,18 +8,38 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->simplePaginate(3);
+    $jobs = Job::with('employer')->latest()->simplePaginate(3);
 
-    return view('jobs', [
+    return view('job.index', compact('jobs'), [
         'jobs' => $jobs
     ]);
+});
+
+
+//that will get errors with wrong request in job-detail becuase first route access whatever jobs/--- string or int
+//solution is make this in upper
+Route::get('/jobs/create', function () {
+
+    return view('job.create');
+});
+Route::post('/jobs', function () {
+    // validation...
+
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1
+    ]);
+
+    return redirect('/jobs');
 });
 
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
-    return view('job', ['job' => $job]);
+    return view('job.show', ['job' => $job]);
 });
+
 
 Route::get('/contact', function () {
     return view('contact');
