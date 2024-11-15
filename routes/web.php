b770c00d09a0cb5fa163eb6659442faf23f32545
Andrew <149857208+PrimeBeyonder@users.noreply.github.com>
2024-11-15 11:33:05 +0630

    auth, resuable component, controllers

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\RegisteredUserController;
+use \App\Http\Controllers\SessionController;
 
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
@@ -10,3 +12,10 @@ Route::resource('jobs', JobController::class);
 
 //route group
 //Route::controller(JobController::class)->group(function () {})
+
+//auth
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
