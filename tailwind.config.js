f7431e962d66683dfdae1d1eae9c45fa6a41953e
Andrew <149857208+PrimeBeyonder@users.noreply.github.com>
2024-11-15 20:03:05 +0630

    introduce vite and tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+    content: ["./resources/**/*.blade.php", "./resources/**/*.js"],
+    theme: {
+        extend: {
+            colors: {
+                black: "#060606",
+            },
+        },
+    },
+    plugins: [],
+};
